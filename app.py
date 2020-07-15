0b0a994609cb24790af219bc13e99c07e7bf538f
Pierre Ravenel <pierreafe@gmail.com>
2020-07-15 19:22:31 +0200

    add session & authentification

--- app.py
+++ app.py
@@ -3,46 +3,73 @@ test flask_socketio
 """
 
 from flask_socketio import SocketIO
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, abort, redirect, url_for
+from flask_login import LoginManager, login_user, current_user, UserMixin, login_required
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'secret!'
 socketio = SocketIO(app)
+login = LoginManager(app)  # Login
+login.login_view = 'loginform'
+
+connected_dict = {}
+
+@login.user_loader
+def user_loader(id):
+    return User(id)
+
+class User(UserMixin):
+    def __init__(self, username):
+        self.id = username
+
+@app.route('/login', methods=['GET', 'POST'])
+def loginform():
+    if request.method == 'POST':
+        print(request.form)
+        username = request.form['username']
+        # password = request.form['password']
+        if username is not None:
+            login_user(User(username))
+            return redirect(request.args.get('next'))
+    return render_template('login.html')
 
 @app.route('/')
+@app.route('/index')
+@login_required
 def index():
     return render_template('index.html') # socketio in it
 
-connected = []
-
-def send_connected():
+def send_connected(data):
     req = {"type":  "connected",
-           "connected": connected}
-    socketio.emit('data', req, callback=lambda _: print("DONE"))
+           "connected": data}
+    socketio.emit('data', req)
 
 def send_msg(user, msg):
     req = {"type": "msg",
            "user": user,
            "msg": msg}
-    socketio.emit('data', req, callback=lambda _: print("DONE"))
+    socketio.emit('data', req)
 
 @socketio.on('connect')
 def connect():
-    connected.append(request.sid)
-    send_connected()
-    print('Client connected')
+    # emit('welcome', {'username': current_user.id})
+    connected_dict[current_user.id] = request.sid # ling username => socket ID
+    send_msg("@console", f" + {current_user.id}")
+    send_connected(connected_dict)
+    print(f'socket connected {current_user.id} => {request.sid}')
 
 @socketio.on('disconnect')
 def disconnect():
-    connected.remove(request.sid)
-    send_connected()
-    print('Client disconnected')
+    connected_dict[current_user.id] = None
+    send_msg("@console", f" - {current_user.id}")
+    send_connected(connected_dict)
+    print(f'socket disconnected {current_user.id} => None')
 
 @socketio.on('my event')
-def handle_my_custom_event(json, methods=['GET', 'POST']):
-    print('my event: ' + str(json))
-    send_msg(request.sid, str(json))
+def handle_my_custom_event(data, methods=['GET', 'POST']):
+    print('my event: ' + str(data))
+    send_msg(str(current_user.id), str(data))
 
 
 if __name__ == '__main__':
-    socketio.run(app, debug=False)
+    socketio.run(app, debug=True)
